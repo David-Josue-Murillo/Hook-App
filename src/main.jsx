f550e12db919e031c66bb2c8ee781ad4c0d4aae8
David Murillo <david.josue.murillo23@gmail.com>
2024-07-30 16:51:21 -0500

    Creación de formulario aplicando usState

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
 import './index.css';
-import { CounterWithCustomHook } from './useState/CounterWithCustomHook';
+//import { HooksApp } from './HooksApp';
+//import { CounterWithCustomHook } from './useState/CounterWithCustomHook';
+import { SimpleForm } from './useEffect/SimpleForm';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>,
 )
