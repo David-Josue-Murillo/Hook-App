4f7a4dd41c8684dc88e888807424da1dc3058373
David Murillo <david.josue.murillo23@gmail.com>
2024-07-30 18:49:30 -0500

    FUncion de reset incluida en el useForm

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -4,13 +4,12 @@ import { useForm } from "../hooks/useForm";
 export const FormWithCustomHook = () => {
 
     
-    const {formState, onInputChange, username, email, password} = useForm(
-        {
+    const {formState, onInputChange, onResetForm, username, email, password} = useForm({
             username: '',
             email: '',
             password: ''
         }
-    )
+    );
 
     useEffect( () => {
         console.log("FormState Changed!!!");
@@ -48,6 +47,8 @@ export const FormWithCustomHook = () => {
                 value={password}
                 onChange={onInputChange}
             />
+
+            <button onClick={ onResetForm } className="btn btn-danger mt-2">Reset</button>
         </>
     )
 }
