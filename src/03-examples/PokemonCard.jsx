d0805736c70da4f00cb496b249e347ab23e5a3cb
David Murillo <david.josue.murillo23@gmail.com>
2024-07-30 22:12:46 -0500

    Probando el useLayoutEffect

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 
-export const PokemonCard = ({id, name, sprites = []}) => {
-  return (
-    <section styel={{height: 200}} className='card'>
-        <h2 className='text-capitalize'>#{id} - {name}</h2>
-        <div>
-            {
-                sprites.map(sprite => (
-                    <img key={sprite} src={sprite} alt={name} className='img-fluid'/>
-                ))
-            }
-        </div>
-    </section>
-  )
+export const PokemonCard = ({ id, name, sprites = [] }) => {
+
+    const h2Ref = useRef();
+    const [boxSize , setBoxSize] = useState({height: 0, width: 0});
+
+    useLayoutEffect(() => {
+      const { height, width } = h2Ref.current.getBoundingClientRect();
+      setBoxSize({height, width});
+    }, [name])
+
+    return (
+        <>
+            <section styel={{ height: 200 }} className='card'>
+                <h3 ref={h2Ref} className='text-capitalize'>#{id} - {name}</h3>
+                <div>
+                    {
+                        sprites.map(sprite => (
+                            <img key={sprite} src={sprite} alt={name} className='img-fluid' />
+                        ))
+                    }
+                </div>
+            </section>
+
+            <code>
+                {JSON.stringify(boxSize)}
+            </code>
+        </>
+    )
 }
