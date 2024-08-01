444e6d36bea3f936ccf3f33c3cd3b8c0d98a19b8
David Murillo <david.josue.murillo23@gmail.com>
2024-07-31 22:55:56 -0500

    Optimizando el uso de componentes con dependencias

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { ShowIncrement } from './ShowIncrement';
 
 export const CallbackHook = () => {
 
     const [counter, setCounter] = useState(10);
 
-    const increment = () => {
-        setCounter(counter + 1);
-    }
+    const incrementar = useCallback(() => {
+        setCounter((value) => value + 1);
+    },[]);
 
     return (
         <>
             <h1>useCallBack Hook: {counter}</h1>
             <hr />
 
-            <ShowIncrement increment={increment} />
+            <ShowIncrement increment={incrementar} />
         </>
     )
 }
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const ShowIncrement = ({ increment }) => {
+export const ShowIncrement = React.memo(({ increment }) => {
 
     console.log('Renderizando componente');
 
     return (
-        <button className='btn btn-primary' onClick={() => increment()}>
+        <button className='btn btn-primary' onClick={() => {increment();}}>
             +1
         </button>
 
     )
-}
+})
