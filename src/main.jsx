de94503b5c9e2930a413e58e8321dafd38284841
David Murillo <david.josue.murillo23@gmail.com>
2024-08-01 21:33:28 -0500

    Preparando el ambiente para el useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import './index.css';
 //import { MemoHook } from './06-memos/MemoHook';
 //import { CallbackHook } from './06-memos/CallbackHook';
 import { Padre } from './07-tarea-memo/Padre';
+import './08-useReducer/intro-reduces';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
