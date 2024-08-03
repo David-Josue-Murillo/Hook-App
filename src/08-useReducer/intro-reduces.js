4d7d71b6bb07075bd2e7bc6e1b034182039e9685
David Murillo <david.josue.murillo23@gmail.com>
2024-08-02 20:15:58 -0500

    Aplicando idea general sobre el uso del useReducer

--- src/08-useReducer/intro-reduces.js
+++ src/08-useReducer/intro-reduces.js
@@ -1,1 +1,32 @@
-console.log('Hellooo');
+
+const initialState = [{
+    id: 1,
+    todo: 'Probando state',
+    done: false
+}];
+
+const todoReducer = (state = initialState, action = {}) => {
+    
+    if(action.type === 'ADD_TODO') {
+        return [...state, action.payload];
+    } 
+
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'Nuevo todo',
+    done: false
+} 
+
+const addTodoAction = {
+    type: 'ADD_TODO',
+    payload: newTodo
+}
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({state: todos});
